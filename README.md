247add0ddf18c8af5e1ca4246084bbbe6bfc2446
lach8@o2.pl <lach8@o2.pl>
2021-12-18 13:19:59 +0100

    Guess the number game

---
+++ README.md
@@ -0,0 +1,8 @@
+# guess_the_number_1
+
+## Rules
+1. Choose a number from 1 to 100 and try to guess what the computer drew.
+2. If your number is too small, you will receive a message: "To small!" and try again.
+3. If your number is too big, you will receive a message: "To big!" and try again.
+4. If you do not enter a number or enter nothing, you will have to try again. 
+5. If your number is correctly, you will receive a message: "You win!".
--- guess_the_number_1.py
+++ guess_the_number_1.py
@@ -1,18 +1,22 @@
-"""Guess the number game"""
 from random import randint
 
-GUESS = False
-NUMBER = randint(1, 100)
 
-while not GUESS:
-    try:
-        user_number = float(input("Guess the number:"))
-        if NUMBER == user_number:
-            GUESS = True
-            print("You win!")
-        elif NUMBER < user_number:
-            print("To big!")
-        elif NUMBER > user_number:
-            print("To small!")
-    except ValueError:
-        print("It's not a number!")
+def guess_the_number():
+    """Function with game."""
+    guess = False
+    number = randint(1, 100)
+    while not guess:
+        try:
+            user_number = float(input("Guess the number:"))
+            if number == user_number:
+                guess = True
+                print("You win!")
+            elif number < user_number:
+                print("To big!")
+            elif number > user_number:
+                print("To small!")
+        except ValueError:
+            print("It's not a number!")
+
+
+if __name__ == '__main__': guess_the_number()
